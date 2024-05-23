49c3882b8e9325a4beaf0fb16fc62a48716c0fd0
ccamdessus <ccamdessus@udesa.edu.ar>
2024-05-23 13:05:45 -0300

    Practica DOM

--- practicadom/dom/js/dom.js
+++ practicadom/dom/js/dom.js
@@ -1,9 +1,23 @@
 
 alert("Bienvenidos a mi sitio.");
 var nombreUsuario = prompt("Por favor, ingrese su nombre:");
-var confirmacion = confirm("¿Está seguro de querer avanzar?");
+var confirmacion = confirm("¿Estas seguro que quieres avanzar?");
 if (confirmacion) {
-    document.querySelector('h2.saludo').textContent = "Qué alegría que quieras continuar tu visita";
+    document.querySelector('h2.saludo').textContent = "Que alegría que quieras continuar tu visita";
 } else 
     document.querySelector('h2.saludo').textContent = "Lamentamos que no quieras continuar tu visita";
     document.querySelector('h1').textContent = "Bienvenido " + nombreUsuario;
+
+    var edad = prompt("Cuantos años tenes?");
+
+edad = Int(edad);
+
+if (edad > 17) {
+
+    alert("Puedes continuar.");
+} else {
+
+    document.querySelector('.container-general').style.display = "none";
+    document.getElementById('accesoDenegado').style.display = "block";
+}
+
